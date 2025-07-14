c213027fee6c6289f64dfb5f2106dcd4547c042a
IgorD <avtosklokyiv@gmail.com>
2025-07-14 14:57:53 +0300

    sl

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
     title: "Profile | NoteHub",
     description:
       "View your profile information and manage your account on NoteHub.",
-    url: `https://09-auth-three.vercel.app/profile`,
+    url: `https://09-auth-alpha.vercel.app/profile`,
     images: [
       {
         url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
